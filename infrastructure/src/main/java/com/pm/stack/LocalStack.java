1b5f63173a6beb3309009ea4dd17fd51ec11bb03
David Odim <odimdavid2@gmail.com>
2026-08-02 00:40:52 -0500

    Create databases in our cloud formation template using RDS

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -1,9 +1,14 @@
 package com.pm.stack;
 
 import software.amazon.awscdk.App;
+import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.InstanceClass;
+import software.amazon.awscdk.services.ec2.InstanceSize;
+import software.amazon.awscdk.services.ec2.InstanceType;
 import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.rds.*;
 
 public class LocalStack extends Stack {
     private final Vpc vpc;
@@ -11,6 +16,10 @@ public class LocalStack extends Stack {
     public LocalStack(final App scope, final String id, final StackProps props) {
         super(scope, id, props); //calls Stack extension
         this.vpc = createVpc();
+
+        DatabaseInstance authServiceDb = createDatabase("AuthServiceDB", "auth-service-db");
+
+        DatabaseInstance patientServiceDb = createDatabase("PatientServiceDB", "patient-service-db");
     }
 
     private Vpc createVpc() {
@@ -18,6 +27,20 @@ public class LocalStack extends Stack {
                 .create(this, "PatientManagementVPC")
                 .vpcName("PatientManagementVPC")
                 .maxAzs(2) //available in 2 zones in teh world
+                .build();
+    }
+
+    private DatabaseInstance createDatabase(String id, String dbName) {
+        return DatabaseInstance.Builder.create(this, id)
+                .engine(DatabaseInstanceEngine.postgres(PostgresInstanceEngineProps.builder()
+                        .version(PostgresEngineVersion.VER_17_2)
+                        .build()))
+                .vpc(vpc)
+                .instanceType(InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO))
+                .allocatedStorage(20)
+                .credentials(Credentials.fromGeneratedSecret("admin_user"))
+                .databaseName(dbName)
+                .removalPolicy(RemovalPolicy.DESTROY) //typically wouldn't do this in prod unless you specify a backup policy as well
                 .build();
     }
 }
